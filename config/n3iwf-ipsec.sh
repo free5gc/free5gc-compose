43635763b0f789c6caff2ddd97306b1c19babd01
abousselmi <bousselmi.ayoub@yahoo.fr>
2021-02-20 07:42:38 +0100

    Update sample configuration files
    Note that the free5GC.conf file is deprecated. Logging config is
    now distributed across all modules config files. This also updates
    the n3wif networking bootstrap script with some info on the
    configured params.

--- config/n3iwf-ipsec.sh
+++ config/n3iwf-ipsec.sh
@@ -2,6 +2,14 @@
 
 ### N3iwf IPSec tunnel configuration
 
-ip l add name ipsec0 type vti local $(hostname -i | awk '{print $1}') remote 0.0.0.0 key 5
-ip a add 10.0.0.1/24 dev ipsec0
-ip l set dev ipsec0 up
+# As per https://github.com/free5gc/free5gc/issues/45#issuecomment-634012712
+#     IKEBindAddress: dynamically computed by $(hostname -i | awk '{print $1}')
+#     IPSecInterfaceMark: 5
+#     IPSecInterfaceAddress: 10.0.0.1
+#     IPSec subnet CIDR: /24
+#     N3IWF tunnel interface: ipsec0
+#     
+
+ip link add name ipsec0 type vti local $(hostname -i | awk '{print $1}') remote 0.0.0.0 key 5
+ip addr add 10.0.0.1/24 dev ipsec0
+ip link set dev ipsec0 up
