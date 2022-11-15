3084b61da5a4ad8c3d24ac0875090df99b79022f
ianchen0119 <ychen.desl@gmail.com>
2022-11-15 08:57:50 +0000

    add release workflow and needed files

---
+++ script/release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+TAG=${1-"lastest"}
+
+make base
+
+NF_LIST="nrf amf smf udr pcf udm nssf ausf n3iwf"
+
+echo "Building docker images"
+for NF in ${NF_LIST}; do
+    cd nf_${NF}
+    docker build --build-arg DEBUG_TOOLS=true -t free5gc/${NF}:${TAG} .
+    docker push free5gc/${NF}:${TAG}
+    cd -
+done
+
+cd webui
+docker build --build-arg DEBUG_TOOLS=true -t free5gc/webconsole:${TAG} .
+docker image ls
+docker push free5gc/webconsole:${TAG}
