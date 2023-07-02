334c6876e797aa0f25aaaf7068600ad0ba1ebe94
pirog-spb <arozhkevich@edgecom.ru>
2023-07-02 17:35:08 +0300

    update upf params

--- nat/busy-poll.sh
+++ nat/busy-poll.sh
@@ -9,13 +9,16 @@ terminate=0
 
 
 iptables -t nat -A POSTROUTING -s 10.60.0.0/16 -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 10.61.0.0/16 -o eth0 -j MASQUERADE
 iptables -A FORWARD -j ACCEPT
 
 #echo "1200 n6if" >> /etc/iproute2/rt_tables
 #ip rule add to 10.1.0.0/16 table n6if
 #ip route add default via upf.free5gc.org dev eth0 table n6if
 
-ip route add 10.60.0.0/16 via `nslookup upf.free5gc.org | awk '/^Address: / { print $2 }'` dev eth0
+#ip route add 10.60.0.0/16 via `nslookup upf.free5gc.org | awk '/^Address: / { print $2 }'` dev eth0
+ip route add 10.60.0.0/16 via `nslookup upfn6.free5gc.org | awk '/^Address: / { print $2 }'` dev eth1
+ip route add 10.61.0.0/16 via `nslookup upfn6.free5gc.org | awk '/^Address: / { print $2 }'` dev eth1
 
 
 while [ $terminate -ne 1 ]
